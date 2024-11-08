cafa429c1eaa9ef9997340b7219e662d99770145
killgorealpha <killgorealpha@protonmail.com>
2024-11-08 11:44:38 -0800

    Lots of automation scripts added, some in progress still

---
+++ HPL/automation/aocc_setup.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+cd /nfs/general/aocc
+
+sudo dnf install aocc-compiler-5.0.0-1.x86_64.rpm -y
---
+++ HPL/automation/cpu_nfs_setup.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+sudo dnf update -y
+sudo dnf install nfs-utils -y
+
+cd /
+
+sudo mkdir -p /nfs/general
+sudo mkdir -p /nfs/home
+
+sudo mount $@:/nfs/general /nfs/general
+sudo mount $@:/home /nfs/home
+
+df -h
+
+sudo tee -a /etc/fstab << END
+$@:/var/nfs/general    /nfs/general   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
+$@:/home               /nfs/home      nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
+END
