6c37b07f120613c2ef676892776235d92d4d007f
killgorealpha <killgorealpha@protonmail.com>
2024-11-11 06:44:53 -0800

    modified:   HPL/automation/the_gamut.sh

--- HPL/automation/the_gamut.sh
+++ HPL/automation/the_gamut.sh
@@ -19,7 +19,7 @@ ssh scc135-cpu0 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -31,7 +31,7 @@ ssh scc135-cpu1 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -43,7 +43,7 @@ ssh scc135-cpu2 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -55,7 +55,7 @@ ssh scc135-cpu3 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -67,7 +67,7 @@ ssh scc135-cpu4 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -79,7 +79,7 @@ ssh scc135-cpu5 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -91,7 +91,7 @@ ssh scc135-cpu6 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -103,7 +103,7 @@ ssh scc135-cpu7 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -115,7 +115,7 @@ ssh scc135-cpu8 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -127,7 +127,7 @@ ssh scc135-cpu9 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -139,7 +139,7 @@ ssh scc135-cpu10 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -151,7 +151,7 @@ ssh scc135-cpu11 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -163,7 +163,7 @@ ssh scc135-cpu12 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -175,7 +175,7 @@ ssh scc135-cpu13 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -187,7 +187,7 @@ ssh scc135-cpu14 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -199,7 +199,7 @@ ssh scc135-cpu15 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -211,7 +211,7 @@ ssh scc135-cpu16 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -223,7 +223,7 @@ ssh scc135-cpu17 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -235,7 +235,7 @@ ssh scc135-cpu18 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -247,7 +247,7 @@ ssh scc135-cpu19 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -259,7 +259,7 @@ ssh scc135-cpu20 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -271,7 +271,7 @@ ssh scc135-cpu21 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -283,7 +283,7 @@ ssh scc135-cpu22 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -295,7 +295,7 @@ ssh scc135-cpu23 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -307,7 +307,7 @@ ssh scc135-cpu24 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -319,7 +319,7 @@ ssh scc135-cpu25 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -331,7 +331,7 @@ ssh scc135-cpu26 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -343,7 +343,7 @@ ssh scc135-cpu27 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -355,7 +355,7 @@ ssh scc135-cpu28 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
@@ -367,7 +367,7 @@ ssh scc135-cpu29 << EOF
   cd ~
   scp -r setup rocky@hostname:/nfs/general/resources/setup
   cd setup; 
-  ./cpu_nfs_setup.sh <login_ip>;
+  ./cpu_nfs_setup.sh "10.0.225.26";
   ./aocc_setup.sh;
   ./spack_setup.sh
   ./slurm_login_setup.sh;
