23c4d84668c43a7402d1e66aa78932c7a6a738a5
killgorealpha <killgorealpha@protonmail.com>
2024-11-12 10:27:20 -0800

    modified:   HPL/automation/network_optimize.sh

--- HPL/automation/network_optimize.sh
+++ HPL/automation/network_optimize.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo touch /etc/sysctl.d/10-netdev_budget.conf
-
-sudo echo -e "net.core.netdev_budget = 600\n net.core.netdev_budget_usecs = 4000" >> /etc/sysctl.d/10-netdev_budget.conf
-
-sudo sysctl -p /etc/sysctl.d/10-netdev_budget.conf
-
 sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
 sudo sysctl -w net.core.rmem_max=1048576
 sudo sysctl -w net.core.wmem_max=1048576
@@ -17,3 +11,10 @@ sudo sysctl -w net.ipv4.tcp_low_latency=1
 sudo sysctl -w net.ipv4.tcp_fin_timeout=10
 sudo sysctl -w net.ipv4.tcp_max_tw_buckets=450000
 sudo sysctl -w net.ipv4.tcp_window_scaling=1
+
+sudo nano /etc/sysctl.d/10-netdev_budget.conf
+
+net.core.netdev_budget = 600
+net.core.netdev_budget_usecs = 4000
+
+sudo sysctl -p /etc/sysctl.d/10-netdev_budget.conf
