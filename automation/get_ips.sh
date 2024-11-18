403f79856d63057b5e5937e3a007d23f0c2b2ae3
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 09:52:59 -0800

    modified:   automation/get_ips.sh 	modified:   automation/namd_setup.sh

--- automation/get_ips.sh
+++ automation/get_ips.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-BASE_HOSTNAME="scc135-cpu"
+## Requires 2 inputs
+
+BASE_HOSTNAME="$1"
 USER="rocky"
-for i in {0..29}; do
+for i in {0..$2}; do
   HOSTNAME="${USER}@${BASE_HOSTNAME}${i}"
    echo "${HOSTNAME}: $(ssh -o StrictHostKeyChecking=no "$HOSTNAME" "hostname -I")"
 done
