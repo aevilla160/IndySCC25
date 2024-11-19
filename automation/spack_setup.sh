06501ba427a40da1e140c4ec294a419f67d8c1b0
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 20:49:09 -0800

    modified:   automation/spack_setup.sh

--- automation/spack_setup.sh
+++ automation/spack_setup.sh
@@ -9,4 +9,4 @@ git clone -c feature.manyFiles=true https://github.com/spack/spack.git ${SPACK_R
 source ${SPACK_ROOT}/share/spack/setup-env.sh
 spack --help
 
-spack compilers find
+spack compiler find
