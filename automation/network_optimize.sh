33797613f30197860822ca209ce5ee48e7b467d4
killgorealpha <killgorealpha@protonmail.com>
2024-11-19 00:43:37 -0800

    modified:   automation/network_optimize.sh 	renamed:    visuals/Adobe Scan Nov 18, 2024 (1).pdf -> documentation/findmycat/Mystery Application.pdf 	new file:   documentation/findmycat/mystery-app.pdf 	new file:   documentation/icon/task-description_IndySCC.pdf 	new file:   documentation/namd/instructions.pdf 	modified:   findmycat/main.py

--- automation/network_optimize.sh
+++ automation/network_optimize.sh
@@ -1,23 +1,55 @@
 #!/usr/bin/env bash
 set -e
 
-sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
-sudo sysctl -w net.core.rmem_max=16777216
-sudo sysctl -w net.core.wmem_max=16777216
-sudo sysctl -w net.core.rmem_default=16777216
-sudo sysctl -w net.core.wmem_default=16777216
-sudo sysctl -w net.ipv4.tcp_rmem='4096 87380 16777216'
-sudo sysctl -w net.ipv4.tcp_wmem='4096 65536 16777216'
-sudo sysctl -w net.core.netdev_max_backlog=30000
-sudo sysctl -w net.ipv4.tcp_mem='16777216 16777216 16777216'
-sudo sysctl -w net.ipv4.tcp_low_latency=1
-sudo sysctl -w net.ipv4.tcp_fin_timeout=10
+sudo echo 'net.core.default_qdisc=fq' >> sudo /etc/sysctl.conf
+sudo echo 'net.ipv4.tcp_congestion_control=bbr' >> sudo /etc/sysctl.conf
+sudo sysctl -p
+
+# sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
+
+sudo sysctl net.core.rmem_max >> default_network.txt
+sudo sysctl -w net.core.rmem_max=536870912
+
+sudo sysctl net.core.wmem_max >> default_network.txt
+sudo sysctl -w net.core.wmem_max=536870912 
+
+# sudo sysctl -w net.core.rmem_default=16777216
+# sudo sysctl -w net.core.wmem_default=16777216
+
+sudo sysctl net.ipv4.tcp_rmem >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_rmem='4096 87380 268435456'
+
+sudo sysctl net.ipv4.tcp_wmem >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_wmem='4096 87380 268435456'
+
+sudo sysctl net.ipv4.tcp_mem >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_mem='268435456 268435456 268435456'
+
+sudo sysctl net.ipv4.tcp_max_tw_buckets >> default_network.txt
 sudo sysctl -w net.ipv4.tcp_max_tw_buckets=450000
-sudo sysctl -w net.ipv4.tcp_window_scaling=1
 
-sudo nano /etc/sysctl.d/10-netdev_budget.conf
+sudo sysctl net.ipv4.tcp_low_latency >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_low_latency=1
 
-net.core.netdev_budget = 600
-net.core.netdev_budget_usecs = 4000
+sudo sysctl net.ipv4.tcp_fin_timeout >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_fin_timeout=10
 
-sudo sysctl -p /etc/sysctl.d/10-netdev_budget.conf
+# sudo sysctl net.ipv4.tcp_window_scaling >> default_network.txt
+# sudo sysctl -w net.ipv4.tcp_window_scaling=1
+
+sudo sysctl net.core.netdev_max_backlog >> default_network.txt
+sudo sysctl -w net.core.netdev_max_backlog=30000
+
+sudo sysctl net.core.netdev_budget >> default_network.txt
+sudo sysctl -w net.core.netdev_budget = 600
+
+sudo sysctl net.core.netdev_budget_usecs >> default_network.txt
+sudo sysctl -w net.core.netdev_budget_usecs = 4000
+
+sudo sysctl net.ipv4.tcp_no_metrics_save >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_no_metrics_save = 1
+
+sudo sysctl net.ipv4.tcp_mtu_probing >> default_network.txt
+sudo sysctl -w net.ipv4.tcp_mtu_probing = 1
+
+/sbin/tc qdisc add dev ethN root fq maxrate 8gbit 
