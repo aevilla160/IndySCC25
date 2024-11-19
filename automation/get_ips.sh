4336829020a8a8346003c32a156aaead9fe18742
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 17:06:27 -0800

    modified:   automation/get_ips.sh

--- automation/get_ips.sh
+++ automation/get_ips.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-## Requires 1 input
+if [ $# -ne 1 ]; then
+    echo "Error: Please provide the number of hosts to check"
+    echo "Usage: $0 <number_of_hosts>"
+    exit 1
+fi
+
+# Store the argument
+NUM_HOSTS=$1
 
 BASE_HOSTNAME="scc135-cpu"
 USER="rocky"
-for i in {0..$1}; do
-  HOSTNAME="${USER}@${BASE_HOSTNAME}${i}"
-   echo "${HOSTNAME}: $(ssh -o StrictHostKeyChecking=no "$HOSTNAME" "hostname -I")"
+
+for i in $(seq 0 $((NUM_HOSTS-1))); do
+    if [ $i -lt 10 ]; then
+        HOSTNAME="${USER}0@${BASE_HOSTNAME}${i}"
+    else
+        HOSTNAME="${USER}@${BASE_HOSTNAME}${i}"
+    fi
+    echo "${HOSTNAME}: $(ssh -o StrictHostKeyChecking=no "${HOSTNAME}" "hostname -I")"
 done
