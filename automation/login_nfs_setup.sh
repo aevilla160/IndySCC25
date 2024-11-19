a531679ce69846efd9b8057a24edc4d0db7b02f5
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 19:33:03 -0800

    modified:   automation/aocc_setup.sh 	modified:   automation/login_nfs_setup.sh 	modified:   automation/the_gamut.sh

--- automation/login_nfs_setup.sh
+++ automation/login_nfs_setup.sh
@@ -6,7 +6,7 @@ sudo dnf install nfs-utils -y
 
 cd /
 
-sudo mkdir /nfs/general/resources/setup -p
+sudo mkdir /nfs/general/resources/setup/scripts -p
 sudo mkdir /nfs/general/logs/iperf -p
 sudo mkdir /nfs/general/tests
 sudo mkdir /nfs/general/slurm/munge -p
@@ -14,71 +14,50 @@ sudo mkdir /nfs/general/mpi
 sudo mkdir /nfs/general/hpl/amd -p
 sudo mkdir /nfs/general/icon
 
-sudo chown -R rocky:rocky /nfs/general
-
 sudo tee -a /etc/exports << END
-/nfs/general        10.0.225.192(rw,sync,no_subtree_check)
-/home               10.0.225.192(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.198(rw,sync,no_subtree_check)
-/home               10.0.225.198(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.131(rw,sync,no_subtree_check)
-/home               10.0.225.131(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.17(rw,sync,no_subtree_check)
-/home               10.0.225.17(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.107(rw,sync,no_subtree_check)
-/home               10.0.225.107(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.75(rw,sync,no_subtree_check)
-/home               10.0.225.75(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.84(rw,sync,no_subtree_check)
-/home               10.0.225.84(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.208(rw,sync,no_subtree_check)
-/home               10.0.225.208(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.247(rw,sync,no_subtree_check)
-/home               10.0.225.247(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.243(rw,sync,no_subtree_check)
-/home               10.0.225.243(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.124(rw,sync,no_subtree_check)
-/home               10.0.225.124(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.223(rw,sync,no_subtree_check)
-/home               10.0.225.223(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.161(rw,sync,no_subtree_check)
-/home               10.0.225.161(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.238(rw,sync,no_subtree_check)
-/home               10.0.225.238(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.70(rw,sync,no_subtree_check)
-/home               10.0.225.70(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.7(rw,sync,no_subtree_check)
-/home               10.0.225.7(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.190(rw,sync,no_subtree_check)
-/home               10.0.225.190(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.186(rw,sync,no_subtree_check)
-/home               10.0.225.186(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.79(rw,sync,no_subtree_check)
-/home               10.0.225.79(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.185(rw,sync,no_subtree_check)
-/home               10.0.225.185(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.203(rw,sync,no_subtree_check)
-/home               10.0.225.203(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.55(rw,sync,no_subtree_check)
-/home               10.0.225.55(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.12(rw,sync,no_subtree_check)
-/home               10.0.225.12(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.196(rw,sync,no_subtree_check)
-/home               10.0.225.196(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.120(rw,sync,no_subtree_check)
-/home               10.0.225.120(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.93(rw,sync,no_subtree_check)
-/home               10.0.225.93(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.13(rw,sync,no_subtree_check)
-/home               10.0.225.13(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.106(rw,sync,no_subtree_check)
-/home               10.0.225.106(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.61(rw,sync,no_subtree_check)
-/home               10.0.225.61(rw,sync,no_root_squash,no_subtree_check)
-/nfs/general        10.0.225.219(rw,sync,no_subtree_check)
-/home               10.0.225.219(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.121(rw,sync,no_subtree_check)
+/home               10.3.143.121(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.121(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.149(rw,sync,no_subtree_check)
+/home               10.3.143.149(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.149(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.99(rw,sync,no_subtree_check)
+/home               10.3.143.99(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.99(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.76(rw,sync,no_subtree_check)
+/home               10.3.143.76(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.76(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.56(rw,sync,no_subtree_check)
+/home               10.3.143.56(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.56(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.188(rw,sync,no_subtree_check)
+/home               10.3.143.188(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.188(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.234(rw,sync,no_subtree_check)
+/home               10.3.143.234(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.234(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.168(rw,sync,no_subtree_check)
+/home               10.3.143.168(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.168(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.39(rw,sync,no_subtree_check)
+/home               10.3.143.39(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.39(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.75(rw,sync,no_subtree_check)
+/home               10.3.143.75(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.75(rw,sync,no_root_squash,no_subtree_check)
+/nfs/general        10.3.143.8(rw,sync,no_subtree_check)
+/home               10.3.143.8(rw,sync,no_root_squash,no_subtree_check)
+/scratch            10.3.143.8(rw,sync,no_root_squash,no_subtree_check)
 END
+
+cd ~/IndySCC24/automation
+cp *.sh /nfs/general/resources/setup/scripts
+cd /nfs/general/resources/setup/scripts
+chmod +x *
+
+cd / 
+
+sudo chown -R rocky:rocky /nfs/general
 
 sudo systemctl restart nfs-server
 sudo systemctl status nfs-server
-
--- automation/the_gamut.sh
+++ automation/the_gamut.sh
@@ -5,15 +5,15 @@ set -e
 
 echo "RUNNING THE GAMUT"
 
-sudo chmod +x *.sh
+cd /nfs/general/resources/setup/scripts
 
 ## Login Node setup ##
-./get_ips.sh >> ips.txt
-./login_nfs_setup.sh
+./get_ips.sh 11 >> ips.txt
+# ./login_nfs_setup.sh
 ./aocc_setup.sh
 ./spack_setup.sh
-./slurm_login_setup.sh
-./mpi_setup.sh
+# ./slurm_login_setup.sh
+# ./mpi_setup.sh
 
 ## CPU Nodes setup ##
 ssh scc135-cpu0 << EOF
@@ -106,7 +106,7 @@ ssh scc135-cpu9 << EOF
   exit
 EOF
 
-ssh scc135-cpu10 << EOF
+ssh scc135-gpu0 << EOF
   hostname
   sudo dnf update -y
   cd /nfs/general/resources/setup
@@ -114,175 +114,3 @@ ssh scc135-cpu10 << EOF
   ./spack_setup.sh
   exit
 EOF
-
-ssh scc135-cpu11 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu12 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu13 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu14 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu15 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu16 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu17 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu18 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu19 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu20 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu21 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu22 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu23 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu24 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu25 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu26 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu27 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu28 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
-ssh scc135-cpu29 << EOF
-  hostname
-  sudo dnf update -y
-  cd /nfs/general/resources/setup
-  ./aocc_setup.sh
-  ./spack_setup.sh
-  exit
-EOF
-
