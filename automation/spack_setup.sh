37a927dbaec97dee1af6079f87ec4ef4951a5c1c
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 20:36:57 -0800

    modified:   automation/spack_setup.sh

--- automation/spack_setup.sh
+++ automation/spack_setup.sh
@@ -3,7 +3,7 @@ set -e
 
 cd ~
 
-export SPACK_ROOT=/path/spack
+export SPACK_ROOT=/home/rocky/spack
 
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git ${SPACK_ROOT}
 source ${SPACK_ROOT}/share/spack/setup-env.sh
