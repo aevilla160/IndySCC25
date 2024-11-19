df2aa0cc040fda9def3fc6b65d22b4dd5ea43c11
killgorealpha <killgorealpha@protonmail.com>
2024-11-18 16:57:54 -0800

    modified:   automation/get_ips.sh

--- automation/get_ips.sh
+++ automation/get_ips.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-## Requires 2 inputs
+## Requires 1 input
 
-BASE_HOSTNAME="$1"
+BASE_HOSTNAME="scc135-cpu"
 USER="rocky"
-for i in {0..$2}; do
+for i in {0..$1}; do
   HOSTNAME="${USER}@${BASE_HOSTNAME}${i}"
    echo "${HOSTNAME}: $(ssh -o StrictHostKeyChecking=no "$HOSTNAME" "hostname -I")"
 done
