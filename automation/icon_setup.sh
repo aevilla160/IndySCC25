fb06e15afb624d1d8b1c4039b4e200ebf87c97ea
DanielXing1103 <112993709+DanielXing1103@users.noreply.github.com>
2024-11-20 14:39:26 -0500

    Update icon_setup.sh

--- automation/icon_setup.sh
+++ automation/icon_setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+source /nfs/general/spack/share/spack/setup-env.sh 
+rm -rf /home/rocky/optimized_icon/
+rm -rf /home/rocky/icon-model/
+mkdir /home/rocky/optimized_icon
+
 set -ex
 
 cd ~
@@ -11,18 +17,18 @@ cd openmpi-4.1.2
 make -j3 all 2>&1 | tee make.out 
 sudo make install 2>&1 | tee install.out
 
+cd /home/rocky/optimized_icon
 sudo dnf install libxml2 libxml2-devel
 git clone https://gitlab.dkrz.de/icon/icon-model.git
 cd icon-model
 git checkout "icon-2024.07-public"
-
 compiler='gcc@11.4.1'
 mpi='openmpi@4.1.2'
 # module load  openmpi/4.1.4-gcc-12.2.0 gcc/12.2.0 netlib-lapack/3.9.1
 MPI_ROOT='/nfs/general/mpi4_1_2'
 CC=$MPI_ROOT/bin/mpicc
 CFLAGS='-g -gdwarf-4 -march=native -mpc64'
-ICON_CFLAGS='-O3'
+ICON_CFLAGS='-O3 -ltl '
 ICON_BUNDLED_CFLAGS='-O2'
 
 ZLIB_ROOT=$(spack find --format='{prefix}' "zlib%${compiler}")
@@ -45,7 +51,7 @@ FYAML_LIBS="-L$FYAML_ROOT/lib -lfyaml"
 ECCODES_ROOT=$(spack find --format='{prefix}' "eccodes%${compiler} +fortran")
 ECCODES_LIBS="-L$ECCODES_ROOT/lib64 -leccodes_f90 -leccodes"
 
-FC=$MPI_ROOT/bin/mpif90
+FC=$MPI_ROOT/bin/mpif90 
 FCFLAGS="-I${HDF5_ROOT}/include -I${NETCDFF_ROOT}/include -I${ECCODES_ROOT}/include -fmodule-private -fimplicit-none -fmax-identifier-length=63 -Wall -Wcharacter-truncation -Wconversion -Wunderflow -Wunused-parameter -Wno-surprising -fall-intrinsics -g -march=native -mpc64"
 common_extra_FCFLAGS='-fbacktrace -fbounds-check -fstack-protector-all -finit-real=nan -finit-integer=-2147483648 -finit-character=127 -O2'
 
@@ -55,11 +61,12 @@ ICON_BUNDLED_FCFLAGS="${common_extra_FCFLAGS} -std=f2008"
 ICON_ECRAD_FCFLAGS="${common_extra_FCFLAGS} -fallow-invalid-boz"
 
 ICON_DACE_FCFLAGS="${common_extra_FCFLAGS} -fallow-argument-mismatch"
-ICON_DACE_PAcatTH='externals/dace_icon'
+ICON_DACE_PATH='externals/dace_icon'
 
 NETLIB_LAPACK_ROOT=$(spack find --format='{prefix}' "netlib-lapack%${compiler}")
 BLAS_LAPACK_LDFLAGS="-L${NETLIB_LAPACK_ROOT}/lib64"
 BLAS_LAPACK_LIBS="-llapack -lblas"
+
 
 LDFLAGS="-L${HDF5_ROOT}/lib -L${NETCDF_ROOT}/lib -L${NETCDFF_ROOT}/lib ${BLAS_LAPACK_LDFLAGS} -L${ECCODES_ROOT}/lib64 -L${FYAML_ROOT}/lib -L/lib64"
 LIBS="-Wl,--disable-new-dtags -Wl,--as-needed ${XML2_LIBS} ${FYAML_LIBS} ${ECCODES_LIBS} ${BLAS_LAPACK_LIBS} ${NETCDFF_LIBS} ${NETCDF_LIBS} ${HDF5_LIBS} ${ZLIB_LIBS} -ldl"
@@ -87,15 +94,22 @@ LDFLAGS="${LDFLAGS}" \
 LIBS="${LIBS}" \
 MPI_LAUNCH="${MPI_LAUNCH}"
 
-make -j16
+make -j25
 
-# spack will complain about packages, copy the ENTIRE name of the missing package, and do spack install <name> and rerun run-config.sh
-./make_runscripts nh_dcmip_tc_52_r2b4
-mkdir grids
-# download and copy the grids
-# example scp -J rocky@scc135-login.see240009.projects.jetstream-cloud.org /mnt/c/Users/zbmzn/Downloads/icon_grid_0005_R02B04_G.nc rocky@10.3.66.59:/home/rocky/july_icon/icon-model/grids/iconR2B04-grid.nc
-cd run
-# modify the exp.nh_dcmip_tc_52_r2b4.run to what we have 
-# modifying: veritcal_cord_table to absolute,  deleting rhotheta_v flxdwswtoa from output_nml, comment out current output_nml and uncomment the other
-cat > exp.nh_dcmip_tc_52_r2b4.run
-./exp.nh_dcmip_tc_52_r2b4.run
+
+touch  /home/rocky/optimized_icon/icon-model/run/hostfile
+cat << EOF > /home/rocky/optimized_icon/icon-model/run/hostfile
+scc135-cpu0 slots=32
+scc135-cpu1 slots=32
+scc135-cpu2 slots=32
+scc135-cpu3 slots=32
+scc135-cpu4 slots=32
+scc135-cpu5 slots=32
+scc135-cpu6 slots=32
+scc135-cpu7 slots=32
+scc135-cpu8 slots=32
+scc135-cpu9 slots=32
+EOF
+
+cp  /nfs/general/data-SC_R2B4/exp.tropical_cyclones_R2B4_SC  /home/rocky/optimized_icon/icon-model/run/
+cp /nfs/general/exp.tropical_cyclones_R2B4_SC.run .
